30370405fd36006e61daab5589ca9b4396988de4
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-11-11 11:03:07 +0000

    tokenizer

--- bpci-ws/src/tokenizer.py
+++ bpci-ws/src/tokenizer.py
@@ -11,4 +11,18 @@ class TokenType(Enum):
 @dataclass
 class Token:
     type: TokenType
-    value: Any
+    value: Any = None
+
+
+class Tokenizer:
+    def __init__(self, code):
+        self.code = code
+        self.ptr = 0
+
+    def next_token(self):
+        char = self.code[self.ptr]
+
+        if char in "0123456789":
+            return Token(TokenType.INT, int(char))
+        elif char == "+":
+            return Token(TokenType.PLUS)
