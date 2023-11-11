3770e407bcde4988043043de1eeb08faf68adba6
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2023-11-11 11:12:36 +0000

    Update

--- bpci-ws/src/tokenizer.py
+++ bpci-ws/src/tokenizer.py
@@ -6,6 +6,9 @@ from typing import Any
 class TokenType(Enum):
     INT = "INT"
     PLUS = "PLUS"
+    MINUS = "MINUS"
+    MUL = "MUL"
+    DIV = "DIV"
     EOF = "EOF"
 
 
@@ -15,6 +18,14 @@ class Token:
     value: Any = None
 
 
+CHARS_AS_TOKENS = {
+    "+": Token(TokenType.PLUS),
+    "-": Token(TokenType.MINUS),
+    "*": Token(TokenType.MUL),
+    "/": Token(TokenType.DIV),
+}
+
+
 class Tokenizer:
     def __init__(self, code):
         self.code = code
@@ -22,12 +33,20 @@ class Tokenizer:
 
     def next_token(self):
         if self.ptr >= len(self.code):
-            return Token(TokenType.EOF)
+            return Token(TokenType.EOF)  # end of file
 
         char = self.code[self.ptr]
         self.ptr += 1
 
         if char in "0123456789":
             return Token(TokenType.INT, int(char))
-        elif char == "+":
-            return Token(TokenType.PLUS)
+        elif token := CHARS_AS_TOKENS.get(char):
+            return token
+
+
+if __name__ == "__main__":
+    tok = Tokenizer("3 + 5")
+    print(tok.next_token())
+    print(tok.next_token())
+    print(tok.next_token())
+    print(tok.next_token())
